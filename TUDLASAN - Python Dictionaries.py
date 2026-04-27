82c59a71042af292422417e0590785bf0daa558c
Tala Louise B. Tudlasan <tbtudlasan@cvisc.pshs.edu.ph>
2026-04-27 15:09:34 +0800

    Add files via upload

---
+++ TUDLASAN - Python Dictionaries.py
@@ -0,0 +1,12 @@
+student = dict(
+    name = input("Enter your name:Lara "),
+    age = input("Enter your age: 13 "),
+    subject = input("Enter your favorite subject: Science ")
+    )
+    
+print("\nStudent Record:")
+print(f"Name: {student['name']}")
+print(f"Age: {student['age']}")
+print(f"Favorite Subject: {student['subject']}")
+
+
